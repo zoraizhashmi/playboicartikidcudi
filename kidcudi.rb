bcb04d667d964778872eb3db534430349ff8b61f
zoraizhashmi <37843506+zoraizhashmi@users.noreply.github.com>
2020-01-17 14:47:47 -0800

    Add files via upload

---
+++ kidcudi.rb
@@ -0,0 +1,21 @@
+# Loading previous buffer contents. Please wait...
+live_loop :loop1 do
+  play:Eb
+  sleep 0.5
+  play:G
+  sleep 0.5
+  play :C5
+  sleep 0.5
+  play :D5
+  sleep 0.5
+  play :Bb
+  sleep 0.35
+  play :C5
+  sleep 0.35
+  play :A
+  sleep 0.5
+  play :F
+  sleep 0.5
+  play :G4
+  sleep 1.25
+end
